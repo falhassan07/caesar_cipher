c1bf3ead520ed8d7d9b617738af8f042d5ff76f8
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-08-25 13:28:44 -0400

    initial commit

---
+++ encryption.py
@@ -0,0 +1,6 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+action = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
+text = input("Enter your message:\n").lower()
+shift = input("Enter shift number:\n").lower()
+
